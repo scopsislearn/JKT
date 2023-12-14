e83f378e661029675be269be66b659b888923073
scopsislearn <150110177+scopsislearn@users.noreply.github.com>
2023-12-14 11:47:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Navigate to the directory containing the pom.xml file
-# This is necessary if your Jenkins workspace structure is different
-cd path/to/your/project
 
 # Run Maven build
 mvn clean install
